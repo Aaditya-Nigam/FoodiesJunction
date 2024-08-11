66a9f10534261d32a593b35966c50855346bafab
Aaditya Nigam <aadityanigam12@gmail.com>
2024-08-11 11:25:50 +0530

    dessert cards appended

---
+++ script.js
@@ -0,0 +1,28 @@
+const dessertContainer=document.querySelector(".desserts_container");
+
+fetch("./data.json")
+.then(res=>{
+    return res.json();
+})
+.then(data=>{
+    data.forEach(element => {
+        console.log(element.name);
+        let div=document.createElement("div");
+        div.classList.add("dessert_card")
+        div.innerHTML=`
+                        <div class="img_container">
+                            <img src="${element.image.desktop}" alt="sweet">
+                            <div class="add_to_cart">
+                                <img src="assets/images/icon-add-to-cart.svg" alt="cart">
+                                <p>Add to Cart</p>
+                            </div>
+                        </div>
+                        <div class="dessert_details">
+                            <h4 class="dessert_category">${element.category}</h4>
+                            <h3 class="dessert_name">${element.name}</h3>
+                            <p class="dessert_price">$${element.price}</p>
+                        </div>
+        `
+        dessertContainer.append(div);
+    });
+})
